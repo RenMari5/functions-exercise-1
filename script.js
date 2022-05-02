045a2267de5668768c034178c5ea8a29766d7287
Renee Paczkowski <rpaczkowski@gmail.com>
2022-05-01 22:45:20 -0400

    Added Area of Square function

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 function getAreaOfCircle(radius) {
-  let rad = radius;
+  const rad = radius;
   return Math.PI * rad * rad;
 }
 
 console.log(getAreaOfCircle(1));
 
 function getCircumferenceOfCircle(radius) {
-  let rad = radius;
+  const rad = radius;
   return 2 * Math.PI * rad;
 }
 
 console.log(getCircumferenceOfCircle(9));
+
+function getAreaOfSquare(side) {
+  const sides = side;
+  return sides * sides;
+}
+
+console.log(getAreaOfSquare(25));
