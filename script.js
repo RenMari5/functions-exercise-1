9742b25d741dd8941e1fe9ee3f4c8ab10fb8328f
Renee Paczkowski <rpaczkowski@gmail.com>
2022-05-01 22:42:10 -0400

    Corrected Area function and added Circumf function

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function getAreaOfCircle(radius) {
   let rad = radius;
-  const area = Math.PI * rad * rad;
+  return Math.PI * rad * rad;
 }
 
-console.log(getAreaOfCircle(15));
+console.log(getAreaOfCircle(1));
+
+function getCircumferenceOfCircle(radius) {
+  let rad = radius;
+  return 2 * Math.PI * rad;
+}
+
+console.log(getCircumferenceOfCircle(9));
